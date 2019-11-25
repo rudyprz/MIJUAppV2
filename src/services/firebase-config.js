5d12bdaa8db3fe6a6762e6fdbc5142137b87e4bd
Rodolfo Perez <53629391+rudyprz@users.noreply.github.com>
2019-11-25 16:39:11 -0600

    Update firebase-config.js

--- src/services/firebase-config.js
+++ src/services/firebase-config.js
@@ -1,10 +1,10 @@
 const firebase = {
-  apiKey: 'AIzaSyB8oODzFH9_uzCiyK5RlWDyLdUg59lb_bo',
-  authDomain: 'miju-ab4be.firebaseapp.com',
-  databaseURL: 'https://miju-ab4be.firebaseio.com',
-  projectId: 'miju-ab4be',
-  storageBucket: 'miju-ab4be.appspot.com',
-  messagingSenderId: '261218989080'
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: ''
 }
 
 export default {
